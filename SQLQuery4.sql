fe8151221c42544e385e41fecee4a273e680bae6
Rahulsingariya <rahulsingariya40912@gmail.com>
2025-12-30 11:52:31 +0530

    Remove unnecessary blank lines in SQLQuery4.sql

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,10 +1,5 @@
-
-
-
 UPDATE Products 
 SET ImageUrl = '/images/products/' + REPLACE(ProductName, ' ', '') + '.jpg';
-
-
 UPDATE Products SET ImageUrl = '/images/products/MacBookProM514-inch.jpg' WHERE ProductName = 'MacBook Pro M5 14-inch';
 UPDATE Products SET ImageUrl = '/images/products/DellXPS15.jpg' WHERE ProductName = 'Dell XPS 15';
 UPDATE Products SET ImageUrl = '/images/products/HPPavilion14.jpg' WHERE ProductName = 'HP Pavilion 14';
