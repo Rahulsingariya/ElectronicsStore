5954461f8ec387b80c2aa3a2485608a1e640a25f
Rahulsingariya <rahulsingariya40912@gmail.com>
2025-12-30 11:51:20 +0530

    Update ImageUrl for all products
    This SQL query updates the ImageUrl for all products based on their names.

--- SQLQuery5.sql
+++ SQLQuery5.sql
@@ -1,5 +1,2 @@
-﻿-- Automatically set image URLs for all products
--- This assumes your image files are named EXACTLY like product names with .jpg extension
-
 UPDATE Products 
 SET ImageUrl = '/images/products/' + ProductName + '.jpg';
